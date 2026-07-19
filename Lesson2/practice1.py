a77205ab396d4023d31d866829534c552919e3d3
Bui Duc Trung <buiductrung.cva@gmail.com>
2026-07-19 14:50:44 +0700

    Lesson 2

--- Lesson2/practice1.py
+++ Lesson2/practice1.py
@@ -1,6 +1,18 @@
 # Bài 1: Tạo lớp Rectangle với các thuộc tính: length, width.  
 # Tạo phương thức tính diện tích và chu vi của hình chữ nhật. 
-
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+    def area(self):
+        return self.length * self.width
+    def perimeter(self):
+        return 2 * (self.length + self.width)
+    
+hcn1 = Rectangle(5, 3)
+print("Diện tích hình chữ nhật:", hcn1.area())
+print("Chu vi hình chữ nhật:", hcn1.perimeter())
+        
 # Bài 2: Tạo lớp BankAccount với các thuộc tính: 
             # account_number: số tài khoản 
             # owner: tên chủ tài khoản
@@ -10,3 +22,45 @@
             # withdraw(amount): rút tiền từ tài khoản
             # display_balance(): hiển thị số dư tài khoản
             # (amount: số tiền nạp/rút theo đơn vị $)
+
+class BankAccount:
+    def __init__(self, account_number, owner, balance):
+        self.account_number = account_number
+        self.owner = owner
+        self.balance = balance
+
+    def display_balance(self):
+        print('===== ACCOUNT INFO =====')
+        print(f"Account Number: {self.account_number}")
+        print(f"Owner: {self.owner}")
+        print(f"Balance: {self.balance}$")
+        print('========================')
+
+    def deposit(self):
+        amount = float(input('Nhập số tiền nạp: $'))
+        if amount <= 0:
+            print('Số tiền nạp không hợp lệ')
+        else:
+            # Cộng tiền vào số dư tài khoản
+            self.balance += amount
+            print(f"Nạp tiền thành công!")
+            # Hiển thị số dư tài khoản sau khi nạp tiền
+            self.display_balance()
+
+    def withdraw(self, amount:float):
+        if amount <= 0:
+            print('Số tiền rút không hợp lệ')
+        elif amount > self.balance:
+            print('Số dư tài khoản không đủ để rút')
+        else:
+            # Trừ tiền từ số dư tài khoản
+            self.balance -= amount
+            print(f"Rút tiền thành công!")
+            # Hiển thị số dư tài khoản sau khi rút tiền
+            self.display_balance()
+
+acc1 = BankAccount('123', 'Vũ Hoàng', 100)
+acc1.display_balance()
+acc1.deposit()
+acc1.withdraw(50)
+acc1.withdraw(5000)  
---
+++ Lesson2/practice2.py
@@ -0,0 +1,45 @@
+# DICTIONARY & MAP
+# Bài 1: Cho 1 danh sách gồm tên của học sinh (viết hoa lộn xộn)
+# Yêu cầu: Dùng map() để chuyển đổi danh sách trên viết hoa tất cả chữ
+# Ví dụ: tRunG -> TRUNG
+
+# Bài 2: Quản lý thông tin sinh viên
+# #Yêu cầu: Tạo một dictionary lưu trữ thông tin sinh viên với các khóa: name, age, và grade. 
+# Thực hiện các thao tác sau:
+# 1. Thêm sinh viên với thông tin name = "John", age = 22, và grade = "A".
+# 2. Cập nhật grade của sinh viên thành "A+".
+# 3. Xóa thông tin age của sinh viên.
+# 4. Kiểm tra xem name có trong dictionary không.
+
+# Bài 3: Đếm Số Lần Xuất Hiện Của Từ Trong Chuỗi
+# Yêu cầu: Viết chương trình nhận vào một chuỗi và trả về một dictionary đếm số lần xuất hiện của mỗi từ trong chuỗi.
+sentence = 'this is a test this is only a test'
+
+# Bài 4: Gộp Hai Dictionary
+# Yêu cầu: Cho hai dictionary A và B. Viết chương trình gộp chúng lại. Nếu một key xuất hiện ở cả hai dictionary, cộng giá trị của chúng lại.
+A = {'a': 100, 'b': 200, 'c': 300}
+B = {'b': 250, 'c': 150, 'd': 400}
+
+# Bài 5: Tìm Key Có Giá Trị Lớn Nhất
+# Yêu cầu: Cho một dictionary có các cặp {key: value}. Viết chương trình để tìm key có giá trị lớn nhất.
+
+# Bài 6: Sắp Xếp Dictionary Theo Giá Trị
+# Yêu cầu: Viết chương trình để sắp xếp một dictionary theo giá trị từ cao đến thấp.
+grade = {
+
+}
+
+# Bài 7: Nhóm Các Phần Tử Theo Giá Trị
+# Yêu cầu: Viết chương trình để nhóm các phần tử của một dictionary dựa trên giá trị của chúng. Ví dụ, các phần tử có cùng giá trị sẽ được đưa vào một danh sách.
+data = {
+    'apple': 1,
+    'banana': 2,
+    'cherry': 2,
+    'bomb': 3,
+    'elderberry': 3
+}
+
+# Bài 8: Tạo Dictionary Từ Danh Sách
+# Yêu cầu: Viết chương trình tạo một dictionary từ hai danh sách: một danh sách chứa key và một danh sách chứa value tương ứng.
+keys = ['apple', 'banana', 'cherry']
+values = [1, 2, 3]
